d9b850a5936e63cddde3f1640d84c17cda2c59be
medYassine-projects <mohamedyassine.ali@esprit.tn>
2023-06-07 15:18:47 +0200

    mongoose fixed

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+mongoose.set('strictQuery', false);
 const userSchema = new mongoose.Schema({
     first_name: { type: String, default: null },
     last_name: { type: String, default: null },
